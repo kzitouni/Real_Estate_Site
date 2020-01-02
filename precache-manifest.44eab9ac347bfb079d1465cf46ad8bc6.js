62976a10fbbe1dbb8dca8fb8ec42a92c3f5b9aa2
kzitouni <zkarim091019@gmail.com>
2020-01-01 20:42:21 -0500

    Updates

---
+++ precache-manifest.44eab9ac347bfb079d1465cf46ad8bc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb08c5a59b05851070c3eab0d3d2ad1d",
+    "url": "/Real_Estate_Site/index.html"
+  },
+  {
+    "revision": "186cff794a6ab04ef43c",
+    "url": "/Real_Estate_Site/static/css/main.48531d70.chunk.css"
+  },
+  {
+    "revision": "c6364794620a52dadc93",
+    "url": "/Real_Estate_Site/static/js/2.5618cf9a.chunk.js"
+  },
+  {
+    "revision": "2a2b5d035b4bda98bc1abd544c071b7a",
+    "url": "/Real_Estate_Site/static/js/2.5618cf9a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "186cff794a6ab04ef43c",
+    "url": "/Real_Estate_Site/static/js/main.6149ee44.chunk.js"
+  },
+  {
+    "revision": "749ffcc393652d8d43a1",
+    "url": "/Real_Estate_Site/static/js/runtime-main.2b652848.js"
+  }
+]);
